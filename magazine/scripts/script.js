5b304b2f32db592392d5ac7d2e9b92ae116f8854
son <ngocsonkt99@gmail.com>
2022-01-17 22:25:47 +0700

    commit1

---
+++ magazine/scripts/script.js
@@ -0,0 +1,31 @@
+﻿var slideIndex;
+       
+        function showSlides() {
+            var i;
+            var slides = document.getElementsByClassName("mySlides");
+            var dots = document.getElementsByClassName("dot");
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+
+            slides[slideIndex].style.display = "block";
+            dots[slideIndex].className += " active";
+            
+            slideIndex++;
+            
+            if (slideIndex > slides.length - 1) {
+                slideIndex = 0
+            }
+          
+            setTimeout(showSlides, 3000);
+        }
+        //mặc định hiển thị slide đầu tiên
+        showSlides(slideIndex = 0);
+
+
+        function currentSlide(n) {
+            showSlides(slideIndex = n);
+        }
